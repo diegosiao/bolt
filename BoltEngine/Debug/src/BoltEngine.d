a32142b8660adf1682bba3bb8c4e3b17d18444fb
Diego Morais <diegosiao@gmail.com>
2015-12-19 09:39:08 -0300

    Exclusion of Debug folder

--- BoltEngine/Debug/src/BoltEngine.d
+++
@@ -1,7 +0,0 @@
-src/BoltEngine.o: ../src/BoltEngine.cpp \
-  /Users/diegomorais/git/bolt/BoltEngine/include/GL/glut.h \
-  /Users/diegomorais/git/bolt/BoltEngine/include/GL/freeglut_std.h
-
-/Users/diegomorais/git/bolt/BoltEngine/include/GL/glut.h:
-
-/Users/diegomorais/git/bolt/BoltEngine/include/GL/freeglut_std.h:
